3450d20a8cbcbd4839ccf9fe3fc715dfe8605a37
baliklayarproductions-glitch <baliklayarproductions@gmail.com>
2026-09-14 01:10:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-const { app, BrowserWindow, screen, ipcMain } = require('electron');
+const { app, BrowserWindow, screen, ipcMain, protocol } = require('electron');
 const { machineIdSync } = require('node-machine-id');
+
+// WAJIB: Membuka kunci keamanan agar Windows mengizinkan pemutaran video lokal
+protocol.registerSchemesAsPrivileged([
+  { scheme: 'safe-file', privileges: { secure: true, bypassCSP: true, stream: true } }
+]);
 
 let operatorWindow, ledWindow;
 
@@ -24,11 +29,7 @@ ipcMain.on('open-led', () => {
   const externalDisplay = displays.find((display) => display.bounds.x !== 0 || display.bounds.y !== 0);
 
   let windowOptions = {
-    webPreferences: { 
-      nodeIntegration: true, 
-      contextIsolation: false, 
-      webSecurity: false // PENTING: Mengizinkan akses file lokal
-    },
+    webPreferences: { nodeIntegration: true, contextIsolation: false, webSecurity: false },
     frame: false, backgroundColor: '#000000'
   };
 
@@ -49,5 +50,19 @@ ipcMain.on('send-to-led', (event, data) => {
   if (ledWindow) ledWindow.webContents.send('update-led', data);
 });
 
-app.whenReady().then(createOperatorWindow);
+app.whenReady().then(() => {
+  // MESIN PENERJEMAH FILE: Menjadikan file apapun bisa dibaca tanpa error spasi
+  protocol.registerFileProtocol('safe-file', (request, callback) => {
+    let url = request.url.replace(/^safe-file:\/\//i, '');
+    url = url.split('?')[0].split('#')[0]; // Bersihkan karakter kotor
+    try {
+      callback({ path: decodeURIComponent(url) });
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  createOperatorWindow();
+});
+
 app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit(); });
